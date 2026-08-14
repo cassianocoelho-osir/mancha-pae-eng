8db426ce6af521355f3b02d0025321abeb078439
cassianocoelho-osir <cassiano.coelho@osirnet.com.br>
2026-08-14 17:32:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ if not df_mapa.empty:
     
     # --- CONSTRUÇÃO DO POLÍGONO UNIFICADO (SBA / UNARY UNION) ---
     # Conversão de 60 metros para graus aproximados em latitude/longitude
-    RAIO_METROS = 3
+    RAIO_METROS = 10
     GRAUS_POR_METRO = 1 / 111139.0
     raio_graus = RAIO_METROS * GRAUS_POR_METRO
 
